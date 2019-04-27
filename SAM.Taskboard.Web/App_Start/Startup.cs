37aeb8d167d5716592919295d7ebddcc150b95b7
Dmitry <d.yuzefchik@gmail.com>
2019-04-27 14:32:39 +0300

    reverted owin startup

--- SAM.Taskboard.Web/App_Start/Startup.cs
+++ SAM.Taskboard.Web/App_Start/Startup.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.DataProtection;
 using Owin;
-using Unity;
-using Unity.Injection;
 
 [assembly: OwinStartup(typeof(SAM.Taskboard.Web.App_Start.Startup))]
 
@@ -14,9 +11,6 @@ namespace SAM.Taskboard.Web.App_Start
     {
         public void Configuration(IAppBuilder app)
         {
-            var container = new UnityContainer();
-            container.RegisterType<IDataProtectionProvider>(new InjectionFactory(c => app.GetDataProtectionProvider()));
-
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
