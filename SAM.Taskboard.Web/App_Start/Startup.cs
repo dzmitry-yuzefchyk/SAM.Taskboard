7af2ad2dfb04427f87333babb0606501367eb494
Dmitry <d.yuzefchik@gmail.com>
2019-04-27 14:17:33 +0300

    azure fix

--- SAM.Taskboard.Web/App_Start/Startup.cs
+++ SAM.Taskboard.Web/App_Start/Startup.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.DataProtection;
 using Owin;
+using Unity;
+using Unity.Injection;
 
 [assembly: OwinStartup(typeof(SAM.Taskboard.Web.App_Start.Startup))]
 
@@ -11,6 +14,8 @@ namespace SAM.Taskboard.Web.App_Start
     {
         public void Configuration(IAppBuilder app)
         {
+            var container = new UnityContainer();
+            container.RegisterType<IDataProtectionProvider>(new InjectionFactory(c => app.GetDataProtectionProvider()));
 
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
