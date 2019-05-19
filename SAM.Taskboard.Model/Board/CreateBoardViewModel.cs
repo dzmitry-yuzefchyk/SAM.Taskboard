a04ba54c98e99b11f4daeb30fc6bb566c9f88d4a
Dmitry <d.yuzefchik@gmail.com>
2019-05-19 15:41:02 +0300

    connection string

---
+++ SAM.Taskboard.Model/Board/CreateBoardViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SAM.Taskboard.Model.Board
+{
+    public class CreateBoardViewModel
+    {
+        [MaxLength(64, ErrorMessage = "Maximum 64 symbols")]
+        [Required(ErrorMessage = "Please specify field")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Please specify field")]
+        [MaxLength(512, ErrorMessage = "Maximum 512 symbols")]
+        public string About { get; set; }
+        [Required(ErrorMessage = "Please specify field")]
+        public CustomRoles AccessToDeleteBoard { get; set; }
+        [Required(ErrorMessage = "Please specify field")]
+        public CustomRoles AccessToChangeProject { get; set; }
+        [Required(ErrorMessage = "Please specify field")]
+        public CustomRoles AccessToCreateBoard { get; set; }
+    }
+}
